2c8b98530ce0359b92d480f74942c9a2468b7917
lodalee <131640279+lodalee@users.noreply.github.com>
2023-11-15 21:46:01 +0900

    BE-init

---
+++ BE-wonseok/src/main/java/com/bewonseok/BeWonseokApplication.java
@@ -0,0 +1,13 @@
+package com.bewonseok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeWonseokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeWonseokApplication.class, args);
+    }
+
+}
