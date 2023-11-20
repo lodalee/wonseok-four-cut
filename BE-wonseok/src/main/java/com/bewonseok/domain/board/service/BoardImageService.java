292148bbecf834e7b0f49b5e49c6553962054038
chaewon <cwlee4633@naver.com>
2023-11-20 16:41:10 +0900

    [feat] : 게시물 수정, 삭제 기능
    [내용] :
    -patch 로 게시물 수정시 일부분만 수정가능하게 구현
    -게시물 삭제시 연결된 img S3에서 삭제

--- BE-wonseok/src/main/java/com/bewonseok/domain/board/service/BoardImageService.java
+++ BE-wonseok/src/main/java/com/bewonseok/domain/board/service/BoardImageService.java
@@ -1,15 +1,11 @@
 package com.bewonseok.domain.board.service;
 
-import com.bewonseok.domain.board.entity.BoardImg;
 import com.bewonseok.global.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,21 +13,10 @@ public class BoardImageService {
 
     private final S3Service s3Service;
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucketName;
-
-    //이미지를 S3에 업로드하고, PostImg 객체를 생성하여 리스트에 추가
-    public List<BoardImg> uploadImagesToS3AndCreateBoardImages(List<MultipartFile> images) throws IOException {
-        List<BoardImg> boardImgs = new ArrayList<>();
-
-        for (MultipartFile photo : images) {
-            if (!photo.isEmpty()) { // 파일이 비어있지 않은 경우에만 업로드 진행
-                String fileName = s3Service.uploadFileToS3(photo, "board_img");
-                String url = s3Service.getURLFromS3("board_img/" + fileName);
-                BoardImg boardImg = new BoardImg(url, null);
-                boardImgs.add(boardImg);
-            }
-        }
-        return boardImgs;
+    // 이미지를 S3에 업로드하고, BoardImage 객체를 생성하여 반환
+    public String uploadImageToS3AndCreateBoardImage(MultipartFile image) throws IOException {
+        String fileName = s3Service.uploadFileToS3(image, "board_img");
+        String url = s3Service.getURLFromS3("board_img/" + fileName);
+        return url;
     }
 }
--- BE-wonseok/src/main/java/com/bewonseok/domain/board/service/BoardService.java
+++ BE-wonseok/src/main/java/com/bewonseok/domain/board/service/BoardService.java
@@ -2,52 +2,86 @@ package com.bewonseok.domain.board.service;
 
 import com.bewonseok.domain.board.dto.request.BoardRequestDto;
 import com.bewonseok.domain.board.entity.Board;
-import com.bewonseok.domain.board.entity.BoardImg;
-import com.bewonseok.domain.board.repository.BoardImgRepository;
 import com.bewonseok.domain.board.repository.BoardRepository;
 import com.bewonseok.domain.user.entity.User;
+import com.bewonseok.global.dto.response.CustomResponseDto;
+import com.bewonseok.global.dto.response.constant.SuccessMessage;
+import com.bewonseok.global.exception.CustomRuntimeException;
+import com.bewonseok.global.exception.constant.ErrorMessage;
+import com.bewonseok.global.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
     private final BoardRepository boardRepository;
-    private final BoardImgRepository boardImgRepository;
     private final BoardImageService boardImageService;
+    private final S3Service s3Service;
 
-    //게시물 등록
-    public void uploadBoard(
-            BoardRequestDto requestDto,
-            List<MultipartFile> images,
-            User user) throws IOException {
-
-        List<BoardImg> boardImgs = null;
-        if (images != null && !images.isEmpty()){
-            boardImgs = boardImageService.uploadImagesToS3AndCreateBoardImages(images);
-        }
-
-        Board board = new Board(requestDto, user);
+    // 게시물 등록
+    public void uploadBoard(BoardRequestDto requestDto, MultipartFile image, User user) throws IOException {
+        String boardImg = boardImageService.uploadImageToS3AndCreateBoardImage(image);
+        Board board = new Board(requestDto, user, boardImg);
         boardRepository.save(board);
-
-        if (boardImgs !=null){
-            associateBoardImagesWithBoard(board, boardImgs);
-        }
     }
 
-    private void associateBoardImagesWithBoard(Board board, List<BoardImg> boardImgs) {
-        for (BoardImg boardImg : boardImgs){
-            boardImg.setBoard(board);
-            boardImgRepository.save(boardImg);
+    public CustomResponseDto updatedBoard(Long boardId, Board updatedBoard, Long userId) {
+
+        Optional<Board> optionalBoard = boardRepository.findById(boardId);
+
+        if (optionalBoard.isPresent()){
+            Board board = optionalBoard.get();
+
+            if (!userId.equals(board.getUser().getId())){
+                throw new CustomRuntimeException(ErrorMessage.NOT_POST_OWNER, HttpStatus.BAD_REQUEST);
+            }
+
+            if (updatedBoard.getTitle() !=null){
+                board.setTitle(updatedBoard.getTitle());
+            }
+
+            if (updatedBoard.getContent() != null){
+                board.setContent(updatedBoard.getContent());
+            }
+
+            boardRepository.save(board);
+
+            return new CustomResponseDto(SuccessMessage.BOARD_UPDATE_SUCCESSFUL, HttpStatus.OK);
+        } else {
+            throw new CustomRuntimeException(ErrorMessage.BOARD_NOT_FOUND, HttpStatus.NOT_FOUND);
         }
     }
 
     //게시물 수정
+    public CustomResponseDto deleteBoard(Long boardId, Long userId) {
+
+        Optional<Board> optionalBoard = boardRepository.findById(boardId);
+
+        if (optionalBoard.isPresent()){
+            Board board = optionalBoard.get();
+
+            if (!userId.equals(board.getUser().getId())){
+                throw new CustomRuntimeException(ErrorMessage.NOT_POST_OWNER, HttpStatus.BAD_REQUEST);
+            }
+
+            String fileName = board.getBoardImg().substring(board.getBoardImg().lastIndexOf("/") + 1);
+            s3Service.deleteFileFromS3("board_img/" + fileName);
+
+            boardRepository.delete(board);
+            return new CustomResponseDto(SuccessMessage.BOARD_DELETE_SUCCESSFUL, HttpStatus.OK);
+        } else {
+            throw new CustomRuntimeException(ErrorMessage.BOARD_NOT_FOUND, HttpStatus.NOT_FOUND);
+        }
+    }
+
+
 
     //게시물 삭제
 
